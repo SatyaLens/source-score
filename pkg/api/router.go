dede35000c647fa1d1c4bf1876301f3ed63bd1b3
Amit Singh <singhamitch@outlook.com>
2024-11-24 18:40:25 +0530

    chore: adds skeleton source domain function
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"log"
 	"net/http"
 	"source-score/pkg/handlers"
 
@@ -15,6 +16,22 @@ func NewRouter() *router {
 	return &router{
 		pingHandler:   handlers.NewPingHandler(),
 	}
+}
+
+func (r *router) CreateSource(ctx *gin.Context) {
+	log.Println("unimplemented")
+}
+
+func (r *router) DeleteSource(ctx *gin.Context, uriDigest string) {
+	log.Println("unimplemented")
+}
+
+func (r *router) GetSource(ctx *gin.Context, uriDigest string) {
+	log.Println("unimplemented")
+}
+
+func (r *router) UpdateSource(ctx *gin.Context, uriDigest string) {
+	log.Println("unimplemented")
 }
 
 func (r *router) GetPing(ctx *gin.Context) {
