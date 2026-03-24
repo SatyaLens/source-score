6b026643c69ecf02f3bc8f3db7e6dfb1dbaa25c0
Amit Singh <singhamitch@outlook.com>
2026-03-24 09:31:24 +0530

    test: adds get all sources tests in repo layer
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- pkg/domain/source/source_repository_test.go
+++ pkg/domain/source/source_repository_test.go
@@ -10,11 +10,35 @@ import (
 
 var _ = Describe("Source model repository layer unit tests", func() {
 	Context("Happy path", Ordered, func() {
-		When("Adding a new source to the DB with valid input", func() {
+		When("Adding new sources to the DB with valid input", func() {
 			It("Should create the source record in the DB", func() {
 				digest, err := sourceRepo.PostSource(context.TODO(), &sampleSourceInput1)
 				Expect(err).ToNot(HaveOccurred())
 				Expect(digest).To(Equal(uriDigest1))
+
+				digest, err = sourceRepo.PostSource(context.TODO(), &sampleSourceInput2)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(digest).To(Equal(uriDigest2))
+			})
+		})
+
+		When("Retrieving all sources from the DB", func() {
+			It("Should return all source records from the DB", func() {
+				sources, err := sourceRepo.GetSources(context.TODO())
+				Expect(err).ToNot(HaveOccurred())
+				Expect(len(sources)).To(Equal(2))
+
+				Expect(sources[0].Name).To(Equal(sampleSourceInput1.Name))
+				Expect(sources[0].Summary).To(Equal(sampleSourceInput1.Summary))
+				Expect(sources[0].Tags).To(Equal(sampleSourceInput1.Tags))
+				Expect(sources[0].Uri).To(Equal(sampleSourceInput1.Uri))
+				Expect(sources[0].UriDigest).To(Equal(uriDigest1))
+
+				Expect(sources[1].Name).To(Equal(sampleSourceInput2.Name))
+				Expect(sources[1].Summary).To(Equal(sampleSourceInput2.Summary))
+				Expect(sources[1].Tags).To(Equal(sampleSourceInput2.Tags))
+				Expect(sources[1].Uri).To(Equal(sampleSourceInput2.Uri))
+				Expect(sources[1].UriDigest).To(Equal(uriDigest2))
 			})
 		})
 
