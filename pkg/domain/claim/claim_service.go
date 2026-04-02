acc7459fc677444dd6060181aecf116e9b02cc98
Amit Singh <singhamitch@outlook.com>
2026-04-02 22:47:05 +0530

    feat: adds claim post and get all functions at service level
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

---
+++ pkg/domain/claim/claim_service.go
@@ -0,0 +1,34 @@
+package claim
+
+import (
+	"context"
+	"source-score/pkg/api"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var (
+	validate = validator.New()
+)
+
+//go:generate go tool counterfeiter . ClaimService
+type ClaimService interface {
+	GetClaims(ctx context.Context) ([]api.Claim, error)
+	PostClaim(ctx context.Context, claimInput *api.ClaimInput) (string, error)
+}
+
+type claimService struct {
+	claimRepo ClaimRepository
+}
+
+func NewClaimService(ctx context.Context, claimRepo ClaimRepository) ClaimService {
+	return &claimService{claimRepo: claimRepo}
+}
+
+func (svc *claimService) GetClaims(ctx context.Context) ([]api.Claim, error) {
+	return svc.claimRepo.GetClaims(ctx)
+}
+
+func (svc *claimService) PostClaim(ctx context.Context, claimInput *api.ClaimInput) (string, error) {
+	return svc.claimRepo.PostClaim(ctx, claimInput)
+}
---
+++ pkg/domain/claim/claim_service_test.go
@@ -0,0 +1,66 @@
+package claim_test
+
+import (
+	"context"
+	"source-score/pkg/api"
+	"source-score/pkg/domain/claim"
+	"source-score/pkg/domain/claim/claimfakes"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	fakeClaimRepo = claimfakes.FakeClaimRepository{}
+	claimSvc      = claim.NewClaimService(context.TODO(), &fakeClaimRepo)
+)
+
+var _ = Describe("Claim model service layer unit tests", Ordered, func() {
+	Context("Happy path", func() {
+		When("Posting a new claim with valid input", func() {
+			It("Should pass data to the repository and return digest", func() {
+				fakeClaimRepo.PostClaimReturnsOnCall(0, claim1Digest, nil)
+				fakeClaimRepo.PostClaimReturnsOnCall(1, claim2Digest, nil)
+
+				input := api.ClaimInput{
+					SourceUriDigest: sampleClaim1.SourceUriDigest,
+					Summary:         &sampleClaim1.Summary,
+					Title:           sampleClaim1.Title,
+					Uri:             sampleClaim1.Uri,
+				}
+				digest, err := claimSvc.PostClaim(context.TODO(), &input)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(digest).To(Equal(claim1Digest))
+				Expect(fakeClaimRepo.PostClaimCallCount()).To(Equal(1))
+				_, arg := fakeClaimRepo.PostClaimArgsForCall(0)
+				Expect(arg).To(Equal(&input))
+
+				input = api.ClaimInput{
+					SourceUriDigest: sampleClaim2.SourceUriDigest,
+					Summary:         &sampleClaim2.Summary,
+					Title:           sampleClaim2.Title,
+					Uri:             sampleClaim2.Uri,
+				}
+				digest, err = claimSvc.PostClaim(context.TODO(), &input)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(digest).To(Equal(claim2Digest))
+				Expect(fakeClaimRepo.PostClaimCallCount()).To(Equal(2))
+				_, arg = fakeClaimRepo.PostClaimArgsForCall(0)
+				Expect(arg).To(Equal(&input))
+			})
+		})
+
+		When("Retrieving all claims", func() {
+			It("Should return claims from repository", func() {
+				expected := []api.Claim{sampleClaim1, sampleClaim2}
+				fakeClaimRepo.GetClaimsReturnsOnCall(0, expected, nil)
+
+				claims, err := claimSvc.GetClaims(context.TODO())
+				Expect(err).ToNot(HaveOccurred())
+				Expect(len(claims)).To(Equal(2))
+				Expect(claims).To(ContainElements(expected))
+				Expect(fakeClaimRepo.GetClaimsCallCount()).To(Equal(1))
+			})
+		})
+	})
+})
