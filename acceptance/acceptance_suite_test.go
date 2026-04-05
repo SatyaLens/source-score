dfd67d85699ab7e181a545d436a6b9ce5aa9687f
Amit Singh <singhamitch@outlook.com>
2026-04-05 22:44:32 +0530

    test: adds happy path ac test for claim model
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- acceptance/acceptance_suite_test.go
+++ acceptance/acceptance_suite_test.go
@@ -19,8 +19,11 @@ type responseBody struct {
 }
 
 const (
-	uriDigest1 = "8649a4126fb4fc9a750f432b729c8477398cf28ca241403b2cd36a6dc841f441"
-	uriDigest2 = "978d81ca657062910f60263c26ce7261e7530e53bfd28aa48748155eb5621868"
+	uriDigest1   = "8649a4126fb4fc9a750f432b729c8477398cf28ca241403b2cd36a6dc841f441"
+	uriDigest2   = "978d81ca657062910f60263c26ce7261e7530e53bfd28aa48748155eb5621868"
+	uriDigest3   = "f70fe06de54dcaa05e3fcda03ae724ad9d8603c04f6cdbd838c0ad4f2e789ba1"
+	claim1Digest = "369d9f3047c66c2e9b5e39693d9de3664b61a36a2d77cd0484fade042350d4a1"
+	claim2Digest = "a96fe15d3040685b06d0c195d54a13692a9002db148498f185babfb6a083f801"
 )
 
 var (
@@ -40,6 +43,12 @@ var (
 		Tags:    "tag2",
 		Uri:     "https://sample-uri-2",
 	}
+	sourceInput3 = api.SourceInput{
+		Name:    "Sample Source 3",
+		Summary: "Sample summary 3",
+		Tags:    "tag2",
+		Uri:     "https://sample-uri-3",
+	}
 	sampleSource1 = api.Source{
 		Name:      "Sample Source 1",
 		Score:     0,
@@ -48,7 +57,6 @@ var (
 		Uri:       "https://sample-uri-1",
 		UriDigest: uriDigest1,
 	}
-
 	sampleSource2 = api.Source{
 		Name:      "Sample Source 2",
 		Score:     0,
@@ -56,6 +64,24 @@ var (
 		Tags:      "tag2",
 		Uri:       "https://sample-uri-2",
 		UriDigest: uriDigest2,
+	}
+	sampleClaim1 = api.Claim{
+		SourceUriDigest: uriDigest3,
+		Summary:         "Sample claim summary 1",
+		Title:           "Sample Claim 1",
+		Uri:             "https://sample-claim-1",
+		UriDigest:       claim1Digest,
+		Checked:         false,
+		Validity:        false,
+	}
+	sampleClaim2 = api.Claim{
+		SourceUriDigest: uriDigest3,
+		Summary:         "Sample claim summary 2",
+		Title:           "Sample Claim 2",
+		Uri:             "https://sample-claim-2",
+		UriDigest:       claim2Digest,
+		Checked:         false,
+		Validity:        false,
 	}
 )
 
---
+++ acceptance/claim_test.go
@@ -0,0 +1,104 @@
+package acceptance_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"source-score/pkg/api"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Claim model tests", func() {
+	// claim endpoints
+	endpoint, err := url.JoinPath(baseUrl, "/api/v1/claim")
+	Expect(err).To(BeNil())
+
+	claimsEndpoint, err := url.JoinPath(baseUrl, "/api/v1/claims")
+	Expect(err).To(BeNil())
+
+	// we'll create a source to attach claims to
+	srcEndpoint, err := url.JoinPath(baseUrl, "/api/v1/source")
+	Expect(err).To(BeNil())
+
+	Context("Happy path tests", Ordered, func() {
+		When("valid POST requests are sent to create claims", func() {
+			It("should create the claims and return their uri digests", func() {
+				// ensure source exists
+				srcBody, err := json.Marshal(sourceInput3)
+				Expect(err).To(BeNil())
+
+				resp, err := http.Post(srcEndpoint, "application/json", bytes.NewBuffer(srcBody))
+				Expect(err).To(BeNil())
+				Expect(resp.StatusCode).To(BeEquivalentTo(http.StatusCreated))
+
+				var srcResp api.CreateSourceResponse
+				err = json.NewDecoder(resp.Body).Decode(&srcResp)
+				Expect(err).To(BeNil())
+				resp.Body.Close()
+
+				claim1 := api.ClaimInput{
+					SourceUriDigest: srcResp.UriDigest,
+					Summary:         &sampleClaim1.Summary,
+					Title:           sampleClaim1.Title,
+					Uri:             sampleClaim1.Uri,
+				}
+				body1, err := json.Marshal(claim1)
+				Expect(err).To(BeNil())
+
+				resp, err = http.Post(endpoint, "application/json", bytes.NewBuffer(body1))
+				Expect(err).To(BeNil())
+				Expect(resp.StatusCode).To(BeEquivalentTo(http.StatusCreated))
+
+				var respBody map[string]string
+				err = json.NewDecoder(resp.Body).Decode(&respBody)
+				Expect(err).To(BeNil())
+				resp.Body.Close()
+				digest1 := respBody["uriDigest"]
+				Expect(digest1).To(Equal(claim1Digest))
+
+				claim2 := api.ClaimInput{
+					SourceUriDigest: srcResp.UriDigest,
+					Summary:         &sampleClaim2.Summary,
+					Title:           sampleClaim2.Title,
+					Uri:             sampleClaim2.Uri,
+				}
+				body2, err := json.Marshal(claim2)
+				Expect(err).To(BeNil())
+
+				resp, err = http.Post(endpoint, "application/json", bytes.NewBuffer(body2))
+				Expect(err).To(BeNil())
+				Expect(resp.StatusCode).To(BeEquivalentTo(http.StatusCreated))
+
+				err = json.NewDecoder(resp.Body).Decode(&respBody)
+				Expect(err).To(BeNil())
+				resp.Body.Close()
+				digest2 := respBody["uriDigest"]
+				Expect(digest2).To(Equal(claim2Digest))
+
+			})
+		})
+
+		When("GET requests are sent to create claims", func() {
+			It("should return all the created claims", func() {
+				resp, err := http.Get(claimsEndpoint)
+				Expect(err).To(BeNil())
+				defer resp.Body.Close()
+				Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+				var claims []api.Claim
+				err = json.NewDecoder(resp.Body).Decode(&claims)
+				Expect(err).To(BeNil())
+				Expect(len(claims)).To(BeNumerically(">=", 2))
+
+				// assert the created claims are present
+				Expect(claims).To(ContainElement(
+					sampleClaim1,
+					sampleClaim2,
+				))
+			})
+		})
+	})
+})
